444ffb5d5c1473ba5fc8182ff0debca95ab3a134
Jonathan Kennel <jonathankennel@Jonathans-MacBook-Air.local>
2022-06-09 09:25:19 -0400

    fix log_lags

--- R/log_lags.R
+++ R/log_lags.R
@@ -20,10 +20,17 @@ log_lags <- function(n, max_time_lag) {
     warning('The number of lags is greater than the maximum time lag')
     return(1:max_time_lag - 1)
   } else {
+    lags <- round(10^seq(0,
+                   log10(max_time_lag),
+                   length.out = n) - 1)
 
-    return(sort(unique(round(10^seq(0,
-                                    log10(max_time_lag),
-                                    length.out = n) - 1))))
+    wh <- which(lags <= 1:n)
+
+    if(length(wh) > 0) {
+      lags[wh] <- wh - 1
+    }
+
+    return(lags)
   }
 
 }
