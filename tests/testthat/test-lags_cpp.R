99dc57042425788d2d9ca9d5f744ee6c4eda3667
Jonathan Kennel <jonathankennel@Jonathans-MacBook-Air.local>
2022-06-18 22:42:49 -0400

    increase coverage

--- tests/testthat/test-lags_cpp.R
+++ tests/testthat/test-lags_cpp.R
@@ -1,7 +1,12 @@
 test_that("lags_cpp works", {
   expect_error(check_lag(5, 10, 0))
-  expect_error(distributed_lag_parallel(rnorm(10), matrix(1:20, ncol = 4), 5, 3, 4))
-  expect_error(distributed_lag_parallel(rnorm(10), matrix(1:20, ncol = 4), 5, 3, -1))
-  expect_error(distributed_lag_parallel(rnorm(10), matrix(1:20, ncol = 4), 7, 5, 4))
+  expect_error(distributed_lag_parallel(rnorm(10), matrix(1:20, ncol = 5), 5, 3, 4))
+  expect_error(distributed_lag_parallel(rnorm(10), matrix(1:20, ncol = 5), 5, 3, -1))
+  expect_error(distributed_lag_parallel(rnorm(10), matrix(1:20, ncol = 5), 7, 5, 4))
+
+
+  expect_equal(nrow(distributed_lag_parallel(rnorm(10), matrix(1:20, ncol = 5), 4, 2, 0)), 5)
+  expect_equal(nrow(distributed_lag_parallel(rnorm(10), matrix(1:20, ncol = 5), 4, 3, 0)), 4)
+
 
 })
