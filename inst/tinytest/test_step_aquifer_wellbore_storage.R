956b1e2c46704be3f682a02f67db5542f543e9e8
Jonathan Kennel <jkennel@uoguelph.ca>
2024-05-16 22:04:31 -0400

    compare barker-herbert and papadopulos cooper

--- inst/tinytest/test_step_aquifer_wellbore_storage.R
+++ inst/tinytest/test_step_aquifer_wellbore_storage.R
@@ -70,6 +70,49 @@ expect_equivalent(frec$result$aquifer_wellbore_storage, m[, 3],
                   tolerance = 5e-4)
 
 
+
+
+formula <- as.formula(flow_rate~time)
+q <- 0.05
+t_1 <- 10000.0
+t_2 <- 1.0
+s_1 <- 1
+s_2 <- 1e-5
+r <- 10
+R <- 0.15
+time <- 10^seq(-4, 4, 0.1)
+dat <- data.frame(time = time)
+
+bh = recipe(formula = formula, data = dat) |>
+  step_aquifer_patch(time = time,
+                     flow_rate = q,
+                     thickness = 1.0,
+                     radius = r,
+                     radius_patch = R,
+                     specific_storage_inner = s_1,
+                     specific_storage_outer = s_2,
+                     hydraulic_conductivity_inner = t_1,
+                     hydraulic_conductivity_outer = t_2,
+                     n_stehfest = 12L) |>
+  plate()
+
+
+pc = recipe(formula = formula, data = dat) |>
+  step_aquifer_wellbore_storage(time = time,
+                                radius_casing = R,
+                                radius_well = R,
+                                radius = r,
+                                flow_rate = q,
+                                hydraulic_conductivity = t_2,
+                                specific_storage = s_2) |>
+  plate()
+
+expect_equivalent(pc$aquifer_wellbore_storage, bh$aquifer_patch,
+                  tolerance = 1e-6,
+                  info = "StepAquiferWellboreStorage is equivalent to StepAquiferPatch with inner and outer zones having the same properties",
+)
+
+
 # bench::mark(
 #
 # times1 <-  10^seq(-6, 3, 0.001),#seq(1, 86400*1, length.out = 1000000)/86400
