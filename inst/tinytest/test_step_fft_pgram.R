99e17924018039de6fdd28733e850d19eeb6a745
Jonathan Kennel <jkennel@uoguelph.ca>
2024-09-27 10:08:45 -0400

    documentation

--- inst/tinytest/test_step_fft_pgram.R
+++ inst/tinytest/test_step_fft_pgram.R
@@ -45,32 +45,32 @@ expect_equivalent(gain, rep(0.2, n_groups),
 
 # step_fft_welch(c(x,y), length_subset = 10, window = hydrorecipes:::window_rectangle(10)) |>
 
-data(kennel_2020)
-formula <- as.formula(datetime~baro+wl+et)
-formula_1 <- as.formula(wl~baro + et)
-
-frec2 = recipe(formula = formula, data = kennel_2020) |>
-  # step_fft_transfer_welch(formula = formula_1, length_subset = 1000,
-  #                         window = hydrorecipes:::window_rectangle(1000),
-  #                         time_step = 60) |>
-  step_fft_transfer_pgram(formula = formula_1, spans = c(3, 3), time_step = 60) |>
-  step_fft_transfer_experimental(formula = formula_1, n_groups = 600,
-                                 spans = c(3,3), taper = 0.05, time_step = 60) |>
-  prep("df") |>
-  bake()
-
-frec2$get_elapsed_times()
-a <- frec2$get_transfer_data(type = "dt")
-
-
-
-library(ggplot2)
-ggplot(a, aes(x = frequency, y = Mod(value), color = id, group = id)) +
-  geom_line(alpha = 0.5) +
-  # geom_point() +
-  facet_wrap(variable~., ncol = 1) +
-  scale_x_log10() +
-  scale_y_continuous(limits = c(0, 1))
+# data(kennel_2020)
+# formula <- as.formula(datetime~baro+wl+et)
+# formula_1 <- as.formula(wl~baro + et)
+#
+# frec2 = recipe(formula = formula, data = kennel_2020) |>
+#   # step_fft_transfer_welch(formula = formula_1, length_subset = 1000,
+#   #                         window = hydrorecipes:::window_rectangle(1000),
+#   #                         time_step = 60) |>
+#   step_fft_transfer_pgram(formula = formula_1, spans = c(3, 3), time_step = 60) |>
+#   step_fft_transfer_experimental(formula = formula_1, n_groups = 600,
+#                                  spans = c(3,3), taper = 0.05, time_step = 60) |>
+#   prep("df") |>
+#   bake()
+#
+# frec2$get_elapsed_times()
+# a <- frec2$get_transfer_data(type = "dt")
+#
+#
+#
+# library(ggplot2)
+# ggplot(a, aes(x = frequency, y = Mod(value), color = id, group = id)) +
+#   geom_line(alpha = 0.5) +
+#   # geom_point() +
+#   facet_wrap(variable~., ncol = 1) +
+#   scale_x_log10() +
+#   scale_y_continuous(limits = c(0, 1))
 
 
 # plot(Mod(fft_transfer_experimental_1)~frequency, a, type = "l", log = "x", ylim = c(0, 1))
